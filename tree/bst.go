c18327a8990772a62e9693f1f3824a978d474a3e
hanzhao <xyzlinux@163.com>
2018-06-18 14:01:00 -0700

    add node delete interface

--- tree/bst.go
+++ tree/bst.go
@@ -12,6 +12,7 @@ binary search tree
 type bstNode struct {
 	left   *bstNode
 	right  *bstNode
+	parent *bstNode
 	key    int
 	value  interface{}
 	height int
@@ -35,16 +36,14 @@ func (bst *BSTree) Insert(key int, value interface{}) {
 	bst.size++
 }
 
-func (bst *BSTree) Find(key int) {
+func (bst *BSTree) Find(key int) (interface{}, bool) {
 	n, ok := find(&bst.root, key)
 	if !ok {
-		return
+		return nil, false
 	}
-	fmt.Printf("key:%d\n", (*n).key)
+	return (*n).key, true
 }
 
-// the pointer of pointer is better
-//func find(root *bstNode, key int) (**bstNode, bool){
 func find(root **bstNode, key int) (**bstNode, bool) {
 	proot := *root
 	if proot == nil {
@@ -59,16 +58,79 @@ func find(root **bstNode, key int) (**bstNode, bool) {
 	return find(&proot.right, key)
 }
 
-func (bst *BSTree) InOrder() {
-	inOrder(bst.root)
+func (bst *BSTree) Del(key int) bool {
+	if bst.root == nil {
+		return true
+	}
+	node, ok := find(&(bst.root), key)
+	if !ok {
+		return true
+	}
+	dstNode := *node
+	if dstNode.left == nil && dstNode.right == nil {
+		*node = nil
+		goto out
+	}
+	delInernalNode(dstNode)
+out:
+	return true
 }
-func inOrder(root *bstNode) {
+
+func delLeftEmpty(node *bstNode) {
+
+}
+
+func delRightEmpty(node *bstNode) {
+	fmt.Printf("\n xxxxxxxxxxxxdel right empty\n")
+	delNode := node
+	child := &node.left
+	dstNode := &node.left
+	for ; child != nil && *child != nil; child = &(*child).right {
+		dstNode = child
+	}
+	targetNode := *dstNode
+	*dstNode = nil
+	if targetNode.left != nil {
+		delNode.left = targetNode.left
+	}
+	fmt.Printf("dstNode:%v\n", targetNode)
+	delNode.value = targetNode.value
+	delNode.key = targetNode.key
+}
+
+func delInernalNode(node *bstNode) {
+	if node.right == nil {
+		delRightEmpty(node)
+		return
+	}
+	//delNode := node
+	child := &node.right
+	dstNode := &node.right
+	for ; child != nil && *child != nil; child = &(*child).left {
+		dstNode = child
+	}
+	targetNode := *dstNode
+	*dstNode = nil
+	node.right = targetNode.right
+	node.value = targetNode.value
+	node.key = targetNode.key
+}
+
+func (bst *BSTree) InOrder() []int {
+	keys := make([]int, 0, bst.size)
+	inOrder(bst.root, &keys)
+	return keys
+}
+
+//TODO: why keys []int doesn't work
+func inOrder(root *bstNode, keys *[]int) {
 	if root == nil {
 		return
 	}
-	inOrder(root.left)
-	fmt.Printf("node:%d\n", root.key)
-	inOrder(root.right)
+	inOrder(root.left, keys)
+	//keys = append(keys, root.key)
+	*keys = append(*keys, root.key)
+	inOrder(root.right, keys)
 }
 
 func (bst *BSTree) PostOrder() {
