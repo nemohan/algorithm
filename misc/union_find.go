f8e84fd9ff072f31147cc846a20f61b2d2bc4f96
hanzhao <xyzlinux@163.com>
2018-10-05 14:38:35 -0700

    add quik-find

--- misc/union_find.go
+++ misc/union_find.go
@@ -1,49 +1,123 @@
 package misc
 
 import (
-	"fmt"
+	"container/list"
 )
 
+/*************************
+ can use array to implement quik-find.
+ however, find all the elements belong the same set will be harder
+ when use this method
+ 0--1--2--3--4--5
+ 0--1--2--3--4--5
+
+ union(2, 3)
+ 0--1--2--3--4--5
+ 0--1--2--2--4--5
+
+ union(0, 5)
+ 0--1--2--3--4--5
+ 0--1--2--2--4--0
+
+****************************/
 type set struct {
-	elements map[int]bool
+	element  int
+	elements *list.List
 }
 
-type UnionFindSet struct {
-	sets map[int]*set
+type QuickFind struct {
+	sets []*set
 }
 
-func MakeSet(src []int) *UnionFindSet {
-	uset := &UnionFindSet{
-		sets: make(map[int]*set, 0),
+func MakeSet(num int) *QuickFind {
+	uset := &QuickFind{
+		sets: make([]*set, num),
 	}
-	for _, e := range src {
-		s := &set{elements: make(map[int]bool, 0)}
-		s.elements[e] = false
-		uset.sets[e] = s
+	for i := 0; i < num; i++ {
+		s := &set{element: i, elements: list.New()}
+		s.elements.PushBack(i)
+		uset.sets[i] = s
 	}
 	return uset
 }
 
-func (uset *UnionFindSet) Find(element int) []int {
+func (uset *QuickFind) IsSameSet(x, y int) bool {
+	sets := uset.sets
+	if sets[x].element == sets[y].element {
+		return true
+	}
+	return false
+}
+
+func (uset *QuickFind) Find(element int) []int {
 	elements := make([]int, 0)
 	s := uset.sets[element]
-	for k, _ := range s.elements {
-		elements = append(elements, k)
+	for e := s.elements.Front(); e != nil; e = e.Next() {
+		elements = append(elements, e.Value.(int))
 	}
 	return elements
 }
 
-func (uset *UnionFindSet) Union(src, dst int) {
-	srcSet := uset.sets[src]
-	dstSet := uset.sets[dst]
-	for k, _ := range dstSet.elements {
-		srcSet.elements[k] = false
-		uset.sets[k] = srcSet
+func (uset *QuickFind) Unite(src, dst int) {
+	if src == dst {
+		return
 	}
+	if src > dst {
+		src, dst = dst, src
+	}
+	dstSet := uset.sets[dst]
+	srcSet := uset.sets[src]
+
+	for e := dstSet.elements.Front(); e != nil; e = e.Next() {
+		i := e.Value.(int)
+		srcSet.elements.PushBack(i)
+		uset.sets[i] = srcSet
+	}
+	uset.sets[dst] = srcSet
 }
 
-func (uset *UnionFindSet) Dump() {
-	for e, s := range uset.sets {
-		fmt.Printf("element:%d  set:%v\n", e, s.elements)
-	}
+func (uset *QuickFind) Dump() {
+	/*
+		for e, s := range uset.sets {
+			fmt.Printf("element:%d  set:%v\n", e, s.elements)
+		}
+	*/
 }
+
+/*
+type UnionFindSet struct {
+	ids  []int
+	size []int
+}
+
+func NewUnionFindSet(num int) *UnionFindSet {
+	ufs := &UnionFindSet{
+		ids:  make([]int, num, num),
+		size: make([]int, 0, num),
+	}
+	for i := 0; i < num; i++ {
+		ufs.ids[i] = i
+	}
+	return ufs
+}
+
+//TODO: path compression
+func (ufs *UnionFindSet) root(i int) int {
+	for ufs.ids[i] != i {
+		i = ufs.ids[i]
+	}
+	return i
+}
+
+func (ufs *UnionFindSet) Find(x, y int) bool {
+	return ufs.root(x) == ufs.root(y)
+}
+
+//TODO: path compression
+func (ufs *UnionFindSet) Unite(x, y int) {
+	rx := ufs.root(x)
+	ry := ufs.root(y)
+
+	ufs.ids[rx] = ry
+}
+*/
--- misc/union_find_test.go
+++ misc/union_find_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 )
 
+/*
 func TestUnion(t *testing.T) {
 	uset := MakeSet([]int{1, 2, 3, 4, 5})
 	uset.Union(1, 2)
@@ -15,4 +16,31 @@ func TestUnion(t *testing.T) {
 		}
 	}
 
+}
+*/
+
+func TestUnion(t *testing.T) {
+	uset := MakeSet(5)
+	uset.Unite(1, 3)
+	if !uset.IsSameSet(1, 3) {
+		t.Fatalf("id 1 and 3 is not in the same set\n")
+	}
+	uset.Unite(0, 4)
+	uset.Unite(0, 3)
+	if !uset.IsSameSet(1, 0) {
+		t.Fatalf("id 1 and 0 is not in the same set\n")
+	}
+}
+
+func TestUnion2(t *testing.T) {
+	uset := MakeSet(5)
+	uset.Unite(3, 1)
+	if !uset.IsSameSet(1, 3) {
+		t.Fatalf("id 1 and 3 is not in the same set\n")
+	}
+	uset.Unite(4, 0)
+	uset.Unite(3, 0)
+	if !uset.IsSameSet(1, 0) {
+		t.Fatalf("id 1 and 0 is not in the same set\n")
+	}
 }
