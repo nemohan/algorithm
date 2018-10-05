4ff0f9da43a202e8d3b08300e41cbe333a7a5a9f
hanzhao <xyzlinux@163.com>
2018-10-04 18:25:06 -0700

    add single-source shortest-paths algorighm, dijkstra

--- graph/graph.go
+++ graph/graph.go
@@ -6,9 +6,16 @@ import (
 )
 
 type vertex struct {
-	id     int
+	id       int
+	list     *list.List
+	next     *vertex
+	prev     *vertex
+	distance int
+}
+
+type edge struct {
+	end    int
 	weight int
-	list   *list.List
 }
 
 type Graph struct {
@@ -23,19 +30,21 @@ func NewGraph() *Graph {
 	}
 }
 
-func (g *Graph) addEdge(head int, tail int) {
+const maxWeight = 100
+
+func (g *Graph) addEdge(head int, tail int, weight int) {
 	v, ok := g.vertexes[head]
 	if !ok {
 		v = &vertex{id: head, list: list.New()}
 		g.vertexes[head] = v
 	}
-	v.list.PushBack(tail)
+	v.list.PushBack(&edge{end: tail, weight: weight})
 }
 
 //no direction
-func (g *Graph) AddEdge(head int, tail int) {
-	g.addEdge(head, tail)
-	g.addEdge(tail, head)
+func (g *Graph) AddEdge(head int, tail int, weight int) {
+	g.addEdge(head, tail, weight)
+	g.addEdge(tail, head, weight)
 }
 
 func (g *Graph) BFSTraverse() {
@@ -60,12 +69,12 @@ func (g *Graph) bfs(queue *list.List) {
 	fmt.Printf("bfs vertex:%d\n", v.id)
 	g.visited[v.id] = true
 	for e := l.Front(); e != nil; e = e.Next() {
-		id := e.Value.(int)
-		if _, ok := g.visited[id]; ok {
+		pe := e.Value.(*edge)
+		if _, ok := g.visited[pe.end]; ok {
 			continue
 		}
-		g.visited[id] = true
-		queue.PushBack(g.vertexes[id])
+		g.visited[pe.end] = true
+		queue.PushBack(g.vertexes[pe.end])
 	}
 	g.bfs(queue)
 }
@@ -88,17 +97,66 @@ func (g *Graph) dfs(v *vertex) {
 		v := tail.Value.(*vertex)
 		l := v.list
 	*/
-
 	fmt.Printf("dfs vertex:%d\n", v.id)
 	g.visited[v.id] = true
 	l := v.list
 	for e := l.Front(); e != nil; e = e.Next() {
-		id := e.Value.(int)
-		if _, ok := g.visited[id]; ok {
+		pe := e.Value.(*edge)
+		if _, ok := g.visited[pe.end]; ok {
 			continue
 		}
 
-		g.dfs(g.vertexes[id])
+		g.dfs(g.vertexes[pe.end])
+	}
+}
+
+func chooseMinDistance(vertexSlice []*vertex) ([]*vertex, *vertex) {
+	min := 0
+	for i := 1; i < len(vertexSlice); i++ {
+		if vertexSlice[i].distance < vertexSlice[min].distance {
+			min = i
+		}
+	}
+	v := vertexSlice[min]
+	vertexSlice = append(vertexSlice[:min], vertexSlice[min+1:]...)
+	return vertexSlice, v
+}
+
+func updateDistance() {
+
+}
+
+func (g *Graph) ShortestPath(start int) {
+	vertexSet := make([]*vertex, 0)
+	for _, v := range g.vertexes {
+		v.distance = maxWeight
+		vertexSet = append(vertexSet, v)
+	}
+	pathTable := make(map[int]*vertex)
+	g.vertexes[start].distance = 0
+	for i := 0; i < len(g.vertexes); i++ {
+		var v *vertex
+		vertexSet, v = chooseMinDistance(vertexSet)
+		pathTable[v.id] = v
+		if v.prev != nil {
+			v.prev.next = v
+		}
+		fmt.Printf("vertex %d\n", v.id)
+		for e := v.list.Front(); e != nil; e = e.Next() {
+			pe := e.Value.(*edge)
+			id := pe.end
+			t := g.vertexes[id]
+			fmt.Printf("v:%d v2:%d dis:%d weight:%d\n", v.id, id, v.distance, pe.weight)
+			if id != v.id && v.distance+pe.weight < t.distance {
+				//fmt.Printf("v:%d v2:%d dis:%d weight:%d\n", v.id, id, v.distance, t.weight)
+				t.distance = v.distance + pe.weight
+				t.prev = v
+			}
+		}
+	}
+
+	for _, v := range pathTable {
+		fmt.Printf("%d distance:%d\n", v.id, v.distance)
 	}
 }
 
--- graph/graph_test.go
+++ graph/graph_test.go
@@ -6,15 +6,26 @@ import (
 
 func TestBFS(t *testing.T) {
 	g := NewGraph()
-	g.AddEdge(1, 2)
-	g.AddEdge(2, 3)
-	g.AddEdge(1, 4)
-	g.AddEdge(4, 3)
+	g.AddEdge(1, 2, 0)
+	g.AddEdge(2, 3, 0)
+	g.AddEdge(1, 4, 0)
+	g.AddEdge(4, 3, 0)
 	g.BFSTraverse()
 	g.DFSTraverse()
 	//g.Dump()
 }
 
+func TestShortestPath(t *testing.T) {
+	g := NewGraph()
+	g.AddEdge(65, 66, 3)
+	g.AddEdge(65, 68, 7)
+	g.AddEdge(66, 67, 4)
+	g.AddEdge(66, 68, 2)
+	g.AddEdge(67, 68, 5)
+	g.AddEdge(67, 69, 6)
+	g.AddEdge(68, 69, 4)
+	g.ShortestPath(65)
+}
 func TestDFS(t *testing.T) {
 
 }
